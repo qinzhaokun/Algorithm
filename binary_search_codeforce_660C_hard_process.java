e48d1e4463eff674851169d127da232ce59bfd1e
Zhaokun Qin <qinzhaokun@qq.com>
2016-06-25 01:00:25 +0800

    Create binary_search_codeforce_660C_hard_process.java

---
+++ binary_search_codeforce_660C_hard_process.java
@@ -0,0 +1,83 @@
+/*
+You are given an array a with n elements. Each element of a is either 0 or 1.
+
+Let's denote the length of the longest subsegment of consecutive elements in a, consisting of only numbers one, as f(a). You can change no more than k zeroes to ones to maximize f(a).
+
+Input
+The first line contains two integers n and k (1 ≤ n ≤ 3·105, 0 ≤ k ≤ n) — the number of elements in a and the parameter k.
+
+The second line contains n integers ai (0 ≤ ai ≤ 1) — the elements of a.
+
+Output
+On the first line print a non-negative integer z — the maximal value of f(a) after no more than k changes of zeroes to ones.
+
+On the second line print n integers aj — the elements of the array a after the changes.
+
+If there are multiple answers, you can print any one of them.
+
+Examples
+input
+7 1
+1 0 0 1 1 0 1
+output
+4
+1 0 0 1 1 1 1
+input
+10 2
+1 0 0 1 0 1 0 1 0 1
+output
+5
+1 0 0 1 1 1 1 1 0 1
+
+*/
+
+
+package binary_search;
+
+import java.util.Scanner;
+
+/**
+ * Created by ABC on 2016/6/19 0019.
+ */
+public class binary_search_codeforce_660C_hard_process {
+
+    static int range;
+    static int start;
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        int [] arr = new int[300005];
+        int [] sum = new int[300005];
+        while(scanner.hasNext()){
+            int n = scanner.nextInt();int k = scanner.nextInt();
+            for(int i = 1;i <= n;i++){
+                arr[i] = scanner.nextInt();
+                sum[i] = (sum[i-1] + (arr[i] == 0 ? 1:0));
+            }
+            int r = n;
+            range = 0;
+            for(int i = 1;i <= n;i++){
+                if(range > r-i+1) break;
+                    find(i,r,sum,k);
+            }
+
+            System.out.println(range);
+            for(int i = start;i < start+range;i++) arr[i] = 1;
+            for(int i = 1;i < n;i++) System.out.print(arr[i] + " ");
+            System.out.println(arr[n]);
+        }
+    }
+
+    public static void find(int l, int r,int [] sum,int k){
+        int base = l;
+        while (l <= r){
+            int mid = ((l+r)>>1);
+            if(sum[mid] - sum[base-1] <= k){
+                if(mid-base+1 > range) {range = mid-base+1;start = base;}
+                l = mid+1;
+            }
+            else{
+                r = mid-1;
+            }
+        }
+    }
+}
